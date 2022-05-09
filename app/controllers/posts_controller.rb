4c24225f429daf635aa2d8dc59ca6bdd1842dd42
ina-gaki <ina.p1192@gmail.com>
2022-05-09 17:14:45 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   #end
 
   def create
-    post = Post.create(content: params[:content]
-    render json:{post: post}
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
